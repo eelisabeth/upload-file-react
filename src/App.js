5e3ff21a8bf6c211681b7d6184dc2dad61b8a4c5
eelisabeth <eelisabeth.pro@gmail.com>
2022-05-01 21:04:22 +0200

    create the uploadHandler function to upload a new file & a remove function if a file already exists

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ function App() {
 
   const [files, setFiles] = useState([{
     name: 'myFile.pdf'
-  }]); 
+  }]);
+
+  const removeFile = (filename) => {
+    setFiles(files.filter(file => file.name !== filename))
+  }; 
+
+  console.log(files);
 
   return (
     <div className="App">
       <p className="title">Upload file</p>
 
-      <FileUpload files={files} setFiles={setFiles} />
+      <FileUpload files={files} setFiles={setFiles} removeFile={removeFile} />
 
     </div>
   );
--- src/FileUpload/FileUpload.jsx
+++ src/FileUpload/FileUpload.jsx
@@ -1,16 +1,42 @@
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
 import React from 'react'; 
 import './FileUpload.scss'; 
 
-const FileUpload = ({ files, setFiles }) => {
+const FileUpload = ({ files, setFiles, removeFile }) => {
 
-    const uploadHandler = () => {};
+    const uploadHandler = (event) => {
+        const file = event.target.files[0]; 
+        // console.log(file);
+        file.isUploading = true; 
+        setFiles([...files, file]); 
+
+        // upload file 
+        const formData = new FormData(); 
+        formData.append(
+            file.name,
+            file,
+            file.name
+        )
+
+        axios.post('http://localhost:8080/upload', formData)
+            .then((res) => {
+                file.isUploading = false;
+                setFiles([...files, file]); 
+            })
+        
+            .catch((err) => {
+                // inform the user
+                console.log(err);
+                removeFile([...files, file])
+            })
+    };
 
     return (
         <div className='file-card'>
             <div className="file-inputs">
-                <input type="file" />
+                <input type="file" onChange={uploadHandler} />
                 <button>
                     <i>
                         <FontAwesomeIcon icon={faPlus} />
